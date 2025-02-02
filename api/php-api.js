671c66e4bdd12dff484fe34a781b187b6e29c53d
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-02-01 19:55:55 -0800

    Create php-api.js

---
+++ api/php-api.js
@@ -0,0 +1,12 @@
+module.exports = async (req, res) => {
+    const exec = require('child_process').exec;
+
+    exec('php /app/api/your-php-file.php', (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return res.status(500).send(`Error executing PHP code: ${stderr}`);
+        }
+
+        res.status(200).send(stdout); 
+    });
+};
