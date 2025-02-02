14aff0c79719094528303488901c7c0996590134
ABRAHAM DWAMENA <120536940+abrahamdw882@users.noreply.github.com>
2025-02-01 19:57:47 -0800

    Update php-api.js

--- api/php-api.js
+++ api/php-api.js
@@ -1,7 +1,7 @@
 module.exports = async (req, res) => {
     const exec = require('child_process').exec;
 
-    exec('php /app/api/arise.php, (error, stdout, stderr) => {
+    exec('php /app/api/arise.php,' (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
             return res.status(500).send(`Error executing PHP code: ${stderr}`);
